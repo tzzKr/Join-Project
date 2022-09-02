322c1ad3cd8e2f7ea1a1dc821eb2f175387b8e36
tzzkr <gerhardbaliet@yahoo.de>
2022-09-02 19:13:12 +0200

    new design comming

--- javaScript/backlog.js
+++
@@ -1,9 +0,0 @@
-function openTaskEditor() {
-    document.getElementById("backlog-task-overview").classList.add("d-none")
-    document.getElementById("backlog-task-editor").classList.remove("d-none")
-}
-
-function closeTaskEditor() {
-    document.getElementById("backlog-task-editor").classList.add("d-none")
-    document.getElementById("backlog-task-overview").classList.remove("d-none")
-}

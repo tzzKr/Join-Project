a9a5ceeaf3037262ee2659b25b376e0c0afd3732
yannikmorjan <yannik.morjan@web.de>
2022-09-14 17:43:57 +0200

    added simple overlay logic

---
+++ javaScript/contacts.js
@@ -0,0 +1,7 @@
+function openOverlay () {
+    document.getElementById('contact-overlay').classList.remove('d-none');
+}
+
+function closeOverlay () {
+    document.getElementById('contact-overlay').classList.add('d-none');
+}

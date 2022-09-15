13896ee9e173f8d48634140dcd51596824e751bc
HosnyFahim <hosny.fahim1990@gmail.com>
2022-09-15 23:45:57 +0200

    AddTask ContactSelection added

--- javaScript/addTask.js
+++ javaScript/addTask.js
@@ -12,7 +12,6 @@ function openSelection() {
     document.getElementById('selectField').setAttribute('onclick', `javascript: closeSelection()`);
 }
 
-
 function closeSelection() {
     document.getElementById('list').classList.add('growOut');
     setTimeout(() => {
@@ -20,4 +19,27 @@ function closeSelection() {
         document.getElementById('list').classList.remove('growOut');
     }, 200);
     document.getElementById('selectField').setAttribute('onclick', `javascript: openSelection()`);
+}
+
+///**      ContactSelection Functions       **///
+
+function openContactSelection() {
+    document.getElementById('listContact').classList.remove('d-none');
+    document.getElementById('listContact').classList.add('growIn');
+
+    setTimeout(() => {
+        document.getElementById('listContact').classList.remove('growIn');
+    }, 200);
+
+    document.getElementById('selectioContactField').setAttribute('onclick', `javascript: closeContactSelection()`);
+}
+
+
+function closeContactSelection() {
+    document.getElementById('listContact').classList.add('growOut');
+    setTimeout(() => {
+        document.getElementById('listContact').classList.add('d-none');
+        document.getElementById('listContact').classList.remove('growOut');
+    }, 200);
+    document.getElementById('selectioContactField').setAttribute('onclick', `javascript: openContactSelection()`);
 }

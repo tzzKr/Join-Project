aac56c1a1eb298d61062c50b8348e66054c3af70
yannikmorjan <yannik.morjan@web.de>
2022-09-19 21:52:59 +0200

    updated overlay

--- javaScript/contacts.js
+++ javaScript/contacts.js
@@ -1,7 +1,9 @@
 function openOverlay () {
-    document.getElementById('contact-overlay').classList.remove('d-none');
+    document.getElementById('page-mask').classList.remove('d-none');
+    // document.getElementById('contact-overlay').classList.remove('d-none');
 }
 
 function closeOverlay () {
-    document.getElementById('contact-overlay').classList.add('d-none');
+    document.getElementById('page-mask').classList.add('d-none');
+    // document.getElementById('contact-overlay').classList.add('d-none');
 }

f2c8f5a8eb423a53dc7f9170d1c71826b3868077
yannikmorjan <yannik.morjan@web.de>
2022-09-20 11:56:25 +0200

    fixed contact overlay

--- javaScript/contacts.js
+++ javaScript/contacts.js
@@ -1,9 +1,9 @@
 function openOverlay () {
     document.getElementById('page-mask').classList.remove('d-none');
-    // document.getElementById('contact-overlay').classList.remove('d-none');
+    document.getElementById('contact-overlay').classList.remove('d-none');
 }
 
 function closeOverlay () {
     document.getElementById('page-mask').classList.add('d-none');
-    // document.getElementById('contact-overlay').classList.add('d-none');
+    document.getElementById('contact-overlay').classList.add('d-none');
 }

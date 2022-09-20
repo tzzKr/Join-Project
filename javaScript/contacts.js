e3fab57666d9493252655d324f113c6831badb10
yannikmorjan <yannik.morjan@web.de>
2022-09-20 13:15:43 +0200

    added edit contact overlay

--- javaScript/contacts.js
+++ javaScript/contacts.js
@@ -7,3 +7,27 @@ function closeOverlay () {
     document.getElementById('page-mask').classList.add('d-none');
     document.getElementById('contact-overlay').classList.add('d-none');
 }
+
+function changeOverlayToEditContact () {
+    document.getElementById('overlay-headline').innerHTML = 'Edit contact';
+    document.getElementById('overlay-subheadline').classList.add('d-none');
+    document.getElementById('overlay-cancel-btn').classList.add('d-none');
+    document.getElementById('overlay-create-btn').classList.add('d-none');
+    document.getElementById('overlay-save-btn').classList.remove('d-none');
+    document.getElementById('overlay-default-user-img').classList.add('d-none');
+    document.getElementById('overlay-user-img').classList.remove('d-none');
+    
+    openOverlay();
+}
+
+function changeOverlayToNewContact () {
+    document.getElementById('overlay-headline').innerHTML = 'Add contact';
+    document.getElementById('overlay-subheadline').classList.remove('d-none');
+    document.getElementById('overlay-cancel-btn').classList.remove('d-none');
+    document.getElementById('overlay-create-btn').classList.remove('d-none');
+    document.getElementById('overlay-save-btn').classList.add('d-none');
+    document.getElementById('overlay-default-user-img').classList.remove('d-none');
+    document.getElementById('overlay-user-img').classList.add('d-none');
+
+    openOverlay();
+}

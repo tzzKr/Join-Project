8b943eefa36f8d9e8e18c386553199353382df78
yannikmorjan <yannik.morjan@web.de>
2022-10-12 21:11:40 +0200

    updated login

--- javaScript/login.js
+++ javaScript/login.js
@@ -3,11 +3,15 @@
  * 
  */
 
- async function login() {
+let actualUser;
+
+async function login() {
     let email = document.getElementById('email');
     let password = document.getElementById('password');
     let user = users.find( u => u.email == email.value && u.password == password.value);
+    actualUser = user.name;
     console.log(user);
+    console.log(actualUser)
     if (user) {
         window.location.href = 'summary.html';
     } else {

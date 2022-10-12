36199ec1bd2e041ddde16853ac529bedfa1063df
yannikmorjan <yannik.morjan@web.de>
2022-10-12 14:57:58 +0200

    added contact rendering

--- javaScript/contacts.js
+++ javaScript/contacts.js
@@ -32,8 +32,54 @@ function changeOverlayToNewContact () {
     openOverlay();
 }
 
-// let contact = [
-//     { 'name': 'Hosny Fahim', 'email': 'hosny@test.com', 'phone': '01234567989' },
-//     { 'name': 'Gerhard Baliet', 'email': 'gerhard@test.com', 'phone': '0123456789' },
-//     { 'name': 'Yannik Morjan', 'email': 'yannik@test.com', 'phone': '0123456789' }
-// ];
+let contacts = [
+    { 'name': 'Hosny Fahim', 'email': 'hosny@test.com', 'phone': '01234567989' },
+    { 'name': 'Gerhard Baliet', 'email': 'gerhard@test.com', 'phone': '0123456789' },
+    { 'name': 'Yannik Morjan', 'email': 'yannik@test.com', 'phone': '0123456789' },
+    { 'name': 'Ännik Morjan', 'email': 'ännik@test.com', 'phone': '0123456789' },
+    { 'name': 'Hosny Fahim', 'email': 'hosny@test.com', 'phone': '01234567989' },
+    { 'name': 'Önnik Morjan', 'email': 'önik@test.com', 'phone': '0123456789' },
+    { 'name': 'Ünnik Morjan', 'email': 'ünnik@test.com', 'phone': '0123456789' }
+];
+
+let orderedContacts = new Array([],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]);
+
+function orderContacts() {
+    for(let i = 0; i < contacts.length; i++) {
+        let letter = contacts[i].name.toLowerCase().toString();
+        letter = letter.replace(/\u00e4/g, "ae").replace(/\u00fc/g, "ue").replace(/\u00f6/g, "oe");
+        letter = letter.slice(0,1);
+        letter = letter.charCodeAt(0) - 97;
+        orderedContacts[letter].push(contacts[i]);
+    }
+    console.log(orderedContacts);
+    renderContactbook();
+}
+
+function renderContactbook() {
+    document.getElementById('contact-book').innerHTML = '';
+    for(let i = 0; i < orderedContacts.length; i++) {
+        if(orderedContacts[i].length > 0) {
+            document.getElementById('contact-book').innerHTML += /*html*/ `
+                <div class="contactList">
+                    <div class="listLetter">${String.fromCharCode(97 + i).toUpperCase()}</div>
+                    <div class="listSeperator"></div>
+            `
+            for(let j = 0; j < orderedContacts[i].length; j++) {
+                document.getElementById('contact-book').innerHTML += /*html*/ `
+                        <div class="listContact">
+                            <div class="listContactInitials bgOr">VM</div>
+                            <div class="listContactInfo">
+                                <span class="listContactName">${orderedContacts[i][j].name}</span>
+                                <span class="listContactEmail">${orderedContacts[i][j].email}</span>
+                            </div>
+                        </div>
+                `
+            }
+            document.getElementById('contact-book').innerHTML += /*html*/ `
+                </div>    
+            `
+        }
+    }
+    console.log(orderedContacts.length)
+}
--- javaScript/script.js
+++ javaScript/script.js
@@ -4,14 +4,13 @@ let guest = [];
 let tasks = [];
 let assignedUser = [];
 let subtasks = [];
-let contacts = [];
 
 
-let users = [
-    { 'name': 'Hosny Fahim', 'email': 'hosny@test.com', 'password': 'test123', 'userImg': "" },
-    { 'name': 'Gerhard Baliet', 'email': 'gerhard@test.com', 'password': 'test456', 'userImg': "" },
-    { 'name': 'Yannik Morjan', 'email': 'yannik@test.com', 'password': 'test789', 'userImg': "" }
-];
+// let users = [
+//     { 'name': 'Hosny Fahim', 'email': 'hosny@test.com', 'password': 'test123', 'userImg': "" },
+//     { 'name': 'Gerhard Baliet', 'email': 'gerhard@test.com', 'password': 'test456', 'userImg': "" },
+//     { 'name': 'Yannik Morjan', 'email': 'yannik@test.com', 'password': 'test789', 'userImg': "" }
+// ];
 
 //**signUp Functions **//
 async function addUser() {
@@ -118,7 +117,7 @@ async function includeHTML() {
             element.innerHTML = 'Page not found';
         }
     }
-    renderTodos()
+    // renderTodos()
 }
 
 async function init() {

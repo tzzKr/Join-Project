ba12c795f7ba4145c9b287187d04d05d0fb17cfe
yannikmorjan <yannik.morjan@web.de>
2022-10-17 20:41:26 +0200

    updated login session

--- javaScript/login.js
+++ javaScript/login.js
@@ -3,14 +3,15 @@
  * 
  */
 
-let actualUser;
+let actualUser = {};
 
 async function login() {
     let email = document.getElementById('email');
     let password = document.getElementById('password');
     let user = users.find( u => u.email == email.value && u.password == password.value);
-    actualUser = user.name;
-    console.log(user);
+    actualUser.name = user.name;
+    actualUser.email = user.email;
+    actualUser.contacts = user.contacts;
     console.log(actualUser)
     if (user) {
         window.location.href = 'summary.html';

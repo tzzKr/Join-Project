760c1a822f9b54299888a378baf4a51e8d14418c
yannikmorjan <yannik.morjan@web.de>
2022-10-18 15:21:56 +0200

    updated summary greeting

--- javaScript/summary.js
+++ javaScript/summary.js
@@ -1,4 +1,25 @@
-function generateGreetingMessage() {
+function renderGreetingMessage() {
     let sessionUser = getSessionUser();
-    document.getElementById('greeting-user').innerHTML = sessionUser.name;
+    let message = getGreeting();
+    if(sessionUser.name == 'guest') {
+        document.getElementById('greeting-message').innerHTML = message;
+        document.getElementById('greeting-user').innerHTML = '';
+    } else {
+        document.getElementById('greeting-message').innerHTML = message + ',';
+        document.getElementById('greeting-user').innerHTML = sessionUser.name;
+    }
+}
+
+function getGreeting() {
+    let time = new Date();
+    time = time.getHours();
+    if(time >= 5 && time < 12) {
+        return 'Good morning';
+    }
+    if(time >= 12 && time < 18) {
+        return 'Good afternoon';
+    }
+    if(time >= 18 && time < 5) {
+        return 'Good evening';
+    }
 }

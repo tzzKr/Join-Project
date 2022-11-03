39a0721c5733c020e7a7fdb38c67d146119e41ea
yannikmorjan <yannik.morjan@web.de>
2022-11-03 12:51:13 +0100

    updated summary to server information

--- javaScript/summary.js
+++ javaScript/summary.js
@@ -1,3 +1,5 @@
+let tasks;
+
 /**
  * Renders an individual greeting message to the user or guest
  * 
@@ -12,6 +14,7 @@ function renderGreetingMessage() {
         document.getElementById('greeting-message').innerHTML = message + ',';
         document.getElementById('greeting-user').innerHTML = sessionUser.name;
     }
+    loadTasksFromServer();
 }
 
 /**
@@ -21,8 +24,9 @@ function renderGreetingMessage() {
  */
 function getGreeting() {
     let time = new Date();
-    time = time.getHours();
     console.log(time)
+    time = time.getHours();
+    
     if(time >= 5 && time < 12) {
         return 'Good morning';
     }
@@ -32,4 +36,42 @@ function getGreeting() {
     if((time >= 18 && time < 24) || (time >= 0 && time < 5)) {
         return 'Good evening';
     }
+}
+
+/**
+ * Loading task array from server
+ *  
+ */
+async function loadTasksFromServer() {
+    await downloadFromServer();
+    tasks = JSON.parse(backend.getItem('tasks')) || [];
+    renderSummaryInformations();
+}
+
+
+/**
+ * Rendering task informations in the summary boards
+ * 
+ */
+function renderSummaryInformations() {
+    document.getElementById('tasksInBoard').innerHTML = tasks.length;
+    document.getElementById('tasksInProgress').innerHTML = getOccurrence('board', 'inProgress');
+    document.getElementById('awaitingFeedback').innerHTML = getOccurrence('boad', 'testing');
+    document.getElementById('tasksUrgent').innerHTML = getOccurrence('prio', 'urgent');
+    //document.getElementById('upcomingDate').innerHTML =
+    document.getElementById('tasksInToDo').innerHTML = getOccurrence('board', 'todo');
+    document.getElementById('tasksInDone').innerHTML = getOccurrence('board', 'done');
+}
+
+/**
+ * Gets you the number how often a value is counted in tasks[i].subelement
+ * 
+ * @param {string} subelement - subelement thats stored inside the task object in the tasks array 
+ * @param {string} value - value that you wanne search for
+ * @returns the count number 
+ */
+function getOccurrence(subelement, value) {
+    let count = 0;
+    tasks.forEach((v) => (v[subelement] === value && count++));
+    return count;
 }

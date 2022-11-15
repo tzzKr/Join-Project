fe3653de9591cbd338baa105c2a0b1b7411732ca
yannikmorjan <yannik.morjan@web.de>
2022-11-15 18:03:17 +0100

    added user initials

--- javaScript/login.js
+++ javaScript/login.js
@@ -9,7 +9,7 @@ async function login() {
     let password = document.getElementById('password');
     let user = users.find( u => u.email == email.value && u.password == password.value);
     if (user) {
-        actualUser = { name: user.name, email: user.email };
+        actualUser = user.name;
         actualUser = JSON.stringify(actualUser);
         sessionStorage.setItem('sessionUser', actualUser);
         window.location.href = 'summary.html';

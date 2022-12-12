6686ce4ca88a36e4e9349309a0ee77201f749309
yannikmorjan <yannik.morjan@web.de>
2022-12-12 16:44:01 +0100

    updated mobile contacts

--- javaScript/contacts.js
+++ javaScript/contacts.js
@@ -32,7 +32,7 @@ function renderContactbook() {
                     <div class="listSeperator"></div>`
             for(let j = 0; j < orderedContacts[i].length; j++) {
                 document.getElementById('contact-book').innerHTML += /*html*/ `
-                        <div class="listContact" onclick="renderContactDetails(${i},${j})">
+                        <div class="listContact" onclick="renderContactDetails(${i},${j}), mobileSwitchToDetail()">
                             <div class="listContactInitials bgOr">VM</div>
                             <div class="listContactInfo">
                                 <span class="listContactName">${orderedContacts[i][j].name}</span>
@@ -249,4 +249,35 @@ function changeOverlayToNewContact() {
             </button>        
         </div>`;
     openOverlay();
+}
+
+/**
+ * Checks if the window size is equal or less than 760px and than execute code in it.
+ * 
+ */
+function mobileOperator() {
+    if(window.innerWidth <= '760') {
+        document.getElementById('to-contactbook-btn').classList.remove('d-none');
+        document.getElementById('content').classList.add('d-none'); 
+    }  
+}
+
+/**
+ * 
+ * 
+ */
+function mobileSwitchToDetail() {
+    if(window.innerWidth <= '760') {
+        document.getElementById('contact-book').classList.add('d-none');
+        document.getElementById('new-contact-btn').classList.add('d-none');
+        document.getElementById('content').classList.remove('d-none');
+    }
+}
+
+function toContactbookBtn() {
+    if(window.innerWidth <= '760') {
+        document.getElementById('content').classList.add('d-none');
+        document.getElementById('contact-book').classList.remove('d-none');
+        document.getElementById('new-contact-btn').classList.remove('d-none');
+    }
 }

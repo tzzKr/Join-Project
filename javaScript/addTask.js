a2ee8f417c0fc532fcccaf8e5dc7c2ca24d031ab
HosnyFahim <hosny.fahim1990@gmail.com>
2023-01-26 20:22:10 +0100

    dateFormat updatet

--- javaScript/addTask.js
+++ javaScript/addTask.js
@@ -348,9 +348,10 @@ function resetFilterImgPriority() {
 function addDate() {
     let date = document.getElementById('date').value;
     date = new Date(date);
-    date = (date.getMonth() + 1) + '.' + date.getDate() + '.' + date.getFullYear();
+    date = date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDate();
     date.toString(date);
     task.dueDate = date;
+    console.log(task.dueDate);
 }
 
 

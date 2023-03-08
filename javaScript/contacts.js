3976c9e10e5d02ecef61b7fce79e6c6a149e0c66
yannikmorjan <yannik.morjan@web.de>
2023-03-08 22:03:52 +0100

    updated contacts

--- javaScript/contacts.js
+++ javaScript/contacts.js
@@ -26,24 +26,46 @@ function renderContactbook() {
     document.getElementById('contact-book').innerHTML = '';
     for(let i = 0; i < orderedContacts.length; i++) {
         if(orderedContacts[i].length > 0) {
-            document.getElementById('contact-book').innerHTML += /*html*/ `
-                <div class="contactList">
-                    <div class="listLetter">${String.fromCharCode(97 + i).toUpperCase()}</div>
-                    <div class="listSeperator"></div>`
+            document.getElementById('contact-book').innerHTML += returnContactLetterSeperator(i)
             for(let j = 0; j < orderedContacts[i].length; j++) {
-                document.getElementById('contact-book').innerHTML += /*html*/ `
-                        <button class="listContact" onclick="renderContactDetails(${i},${j}), mobileSwitchToDetail()">
-                            <div id="single-contact-init${orderedContacts[i][j].id}" class="listContactInitials">${orderedContacts[i][j].initials}</div>
-                            <div class="listContactInfo">
-                                <span class="listContactName">${orderedContacts[i][j].name}</span>
-                                <span class="listContactEmail">${orderedContacts[i][j].email}</span>
-                            </div>
-                        </button>`;
+                document.getElementById('contact-book').innerHTML += returnContactElement(i,j);
                 document.getElementById(`single-contact-init${orderedContacts[i][j].id}`).style.backgroundColor = orderedContacts[i][j].color;
             }
             document.getElementById('contact-book').innerHTML += /*html*/ `</div>`
         }
     }
+}
+
+/**
+ * It returns a Letter and stroke seperator for the Contacts in the Contactbook.
+ * 
+ * @param i - The index of the letter in the alphabet.
+ * @returns A string of HTML code.
+ */
+function returnContactLetterSeperator(i) {
+    return /*html*/ `
+        <div class="contactList">
+            <div class="listLetter">${String.fromCharCode(97 + i).toUpperCase()}</div>
+            <div class="listSeperator">
+        </div>`
+}
+
+/**
+ * It returns a Contact Element for the Contactbook.
+ *
+ * @param i - the index of the alphabetically orderd contacts
+ * @param j - the index of the contact in the alphabetically orderd contacts array
+ * @returns A string of HTML code.
+ */
+function returnContactElement(i,j) {
+    return /*html*/ `
+        <button class="listContact" onclick="mobileSwitchToDetail(), renderContactDetails(${i},${j})">
+            <div id="single-contact-init${orderedContacts[i][j].id}" class="listContactInitials">${orderedContacts[i][j].initials}</div>
+            <div class="listContactInfo">
+                <span class="listContactName">${orderedContacts[i][j].name}</span>
+                <span class="listContactEmail">${orderedContacts[i][j].email}</span>
+            </div>
+        </button>`;
 }
 
 /**
@@ -86,6 +108,9 @@ function renderContactDetails(firstIndex, secondIndex) {
         addContactanimation();
 }
 
+/**
+ * It adds a class to animate an element, then removes it after a set amount of time.
+ */
 function addContactanimation() {
     document.getElementById('contact-details').classList.add('contactDetailAnimation');
     setTimeout(() => {
@@ -161,17 +186,25 @@ async function deleteContact() {
     let inputPhone = document.getElementById('input-phone').value;
     let index = contacts.indexOf(contacts.find( u => u.name == inputName && u.email == inputEmail && u.phone == inputPhone));
     contacts.splice(index,1);
+    await deletContactFromServer();
+    loadContactsFromServer();
+    document.getElementById('contact-details').innerHTML = '';
+    mobileSwitchToContactbook();
+    initMsgBox('Contact succesfully deleted');
+    closeOverlay();  
+}
+
+/**
+ * Overrides the users object from the server with the chenged one.
+ * 
+ */
+async function deletContactFromServer() {
     let users = JSON.parse(backend.getItem('users')) || [];
     let username = sessionStorage.getItem('sessionUser');
     let user = users.find( u => u.name == JSON.parse(username));
     let userIndex = users.indexOf(user);
     users[userIndex].contacts = contacts;
     await backend.setItem('users', JSON.stringify(users));
-    loadContactsFromServer();
-    document.getElementById('contact-details').innerHTML = '';
-    mobileSwitchToContactbook();
-    initMsgBox('Contact succesfully deleted');
-    closeOverlay();  
 }
 
 /**
@@ -186,13 +219,7 @@ async function saveContact(firstIndex, secondIndex) {
     contacts[id].email = document.getElementById('input-email').value;
     contacts[id].phone = document.getElementById('input-phone').value;
     contacts[id].initials = getInitials(document.getElementById('input-name').value);
-    await downloadFromServer();
-    let users = JSON.parse(backend.getItem('users')) || [];
-    let username = sessionStorage.getItem('sessionUser');
-    let user = users.find( u => u.name == JSON.parse(username));
-    let index = users.indexOf(user);
-    users[index].contacts = contacts;
-    await backend.setItem('users', JSON.stringify(users));
+    await pushEditedContactToServer();
     loadContactsFromServer();
     document.getElementById('contact-details').innerHTML = '';
     mobileSwitchToContactbook();
@@ -201,8 +228,22 @@ async function saveContact(firstIndex, secondIndex) {
 }
 
 /**
- * Create a new contact
- * 
+ * It downloads the users from the server, finds the user that is currently logged in, finds the index
+ * of that user in the array, and then sets the contacts of that user to the contacts array.
+ */
+async function pushEditedContactToServer() {
+    await downloadFromServer();
+    let users = JSON.parse(backend.getItem('users')) || [];
+    let username = sessionStorage.getItem('sessionUser');
+    let user = users.find( u => u.name == JSON.parse(username));
+    let index = users.indexOf(user);
+    users[index].contacts = contacts;
+    await backend.setItem('users', JSON.stringify(users));
+}
+
+/**
+ * It takes the values from the input fields, checks if the contact already exists, and if not, it
+ * pushes the contact to the server and reloads the contacts from the server.
  */
 async function createContact() {
     let inputName = document.getElementById('input-name').value;
@@ -212,21 +253,32 @@ async function createContact() {
     if(existingContact) {
         initMsgBox('Contact already exists!');
     } else {
-        await downloadFromServer();
-        let users = JSON.parse(backend.getItem('users')) || [];
-        let username = sessionStorage.getItem('sessionUser');
-        let user = users.find( u => u.name == JSON.parse(username));
-        let index = users.indexOf(user);
-        let serverContacts = users[index].contacts;
-        serverContacts.push({name: inputName, email: inputEmail, phone: inputPhone, color: colorRange[Math.floor(Math.random() * colorRange.length)], initials: getInitials(inputName)});
-        users[index].contacts = serverContacts;
-        await backend.setItem('users', JSON.stringify(users));
+        await pushNewContactToServer(inputName, inputEmail, inputPhone);
         loadContactsFromServer();
         initMsgBox('Contact succesfully created');
         closeOverlay();
     }
-    
 }
+
+/**
+ * It downloads the users from the server, finds the user that is currently logged in, pushes a new
+ * contact to the user's contacts array, and then uploads the users back to the server.
+ * @param inputName - name of the contact
+ * @param inputEmail - email of the contact
+ * @param inputPhone - phonenumber of the contact
+ */
+async function pushNewContactToServer(inputName, inputEmail, inputPhone) {
+    await downloadFromServer();
+    let users = JSON.parse(backend.getItem('users')) || [];
+    let username = sessionStorage.getItem('sessionUser');
+    let user = users.find( u => u.name == JSON.parse(username));
+    let index = users.indexOf(user);
+    let serverContacts = users[index].contacts;
+    serverContacts.push({name: inputName, email: inputEmail, phone: inputPhone, color: colorRange[Math.floor(Math.random() * colorRange.length)], initials: getInitials(inputName)});
+    users[index].contacts = serverContacts;
+    await backend.setItem('users', JSON.stringify(users));
+}
+
 /**
  * Load contacts from server
  * 
@@ -286,16 +338,8 @@ function changeOverlayToNewContact() {
 }
 
 /**
- * Checks if the window size is equal or less than 1100px and than execute code in it.
- * 
- */
-function mobileOperator() {
-    mobileSwitchToContactbook();
-}
-
-/**
- * 
- * 
+ * If the window width is less than or equal to 1100px, hide the contact book and set its z-index to 0,
+ * hide the new contact button and set the content div's display to flex and its z-index to 1.
  */
 function mobileSwitchToDetail() {
     if(window.innerWidth <= '1100') {
@@ -306,12 +350,15 @@ function mobileSwitchToDetail() {
         document.getElementById('content').style.zIndex = 1;
     } 
     else {
-        document.getElementById('content').style.display = 'flex';
-        document.getElementById('contact-book').style.display = 'block';
-        document.getElementById('new-contact-btn').style.display = 'flex';
+        resetMobile();
     }
 }
 
+/**
+ * If the window width is less than or equal to 1100px, then set the z-index of the content div to 0,
+ * set the display of the content div to none, set the display of the contact-book div to block, set
+ * the z-index of the contact-book div to 1, and set the display of the new-contact-btn div to flex.
+ */
 function mobileSwitchToContactbook() {
     if(window.innerWidth <= '1100') {
         document.getElementById('content').style.zIndex = 0;
@@ -321,9 +368,17 @@ function mobileSwitchToContactbook() {
         document.getElementById('new-contact-btn').style.display = 'flex';
     } 
     else {
-        document.getElementById('content').style.display = 'flex';
-        document.getElementById('contact-book').style.display = 'block';
-        document.getElementById('new-contact-btn').style.display = 'flex';
+        resetMobile();
         document.getElementById('contact-details').innerHTML = '';
     }
+}
+
+/**
+ * Resets the Mobile Version to Desktop Version
+ * 
+ */
+function resetMobile() {
+    document.getElementById('content').style.display = 'flex';
+    document.getElementById('contact-book').style.display = 'block';
+    document.getElementById('new-contact-btn').style.display = 'flex';
 }

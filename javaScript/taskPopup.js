c39be142ad281f42c5e41e3be05e31b59fd55323
HosnyFahim <hosny.fahim1990@gmail.com>
2023-03-17 13:17:38 +0100

    addTask popUp responsive fixed

--- javaScript/taskPopup.js
+++ javaScript/taskPopup.js
@@ -1,6 +1,8 @@
 function openTaskPopUp() {
     document.getElementById('task-popUp').classList.remove('d-none');
     document.getElementById('task-bgr-popUp').classList.remove('d-none');
+    document.getElementById('task-popUp').classList.add('animationFadeInLeft');
+    document.getElementById('task-bgr-popUp').classList.add('animationFadeInLeft');
 }
 
 function closeTaskPopUp() {

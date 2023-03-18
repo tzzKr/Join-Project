c648892027d941e7b809fe4f8d59130c7d0d2ebd
yannikmorjan <yannik.morjan@web.de>
2023-03-18 20:02:34 +0100

    updated addtask popup

--- javaScript/taskPopup.js
+++ javaScript/taskPopup.js
@@ -9,3 +9,28 @@ function closeTaskPopUp() {
     document.getElementById('task-popUp').classList.add('d-none');
     document.getElementById('task-bgr-popUp').classList.add('d-none');
 }
+
+function getSelectedContact(i,j) {
+    let email = orderedContacts[i][j].email;
+    let index = contacts.indexOf(contacts.find( u => u.email == email));
+    if(!document.getElementById('checkboxAssignedTo'+ (index + 1)).checked)
+        checkClick('checkboxAssignedTo'+ (index + 1), index);
+}
+
+function resetTask() {
+    task = {
+        id: "",
+        board: "todo",
+        category: "",
+        categoryColor: "",
+        title: "",
+        description: "",
+        progress: 0,
+        progressNumber: 0,
+        assignedTo: new Array,
+        prio: "",
+        dueDate: "",
+        subtasks: new Array
+    }
+    setDate();
+}
